38a40c621b459a10d9b7e49e5d07ff82a6500443
wallyali <abdishwak@gmail.com>
2013-12-23 23:09:01 -0600

    Add tip calculator version1

---
+++ tip_calculator/tip_calculator1.rb
@@ -0,0 +1,14 @@
+meal 	 = 15.00
+tax_rate = 7
+tip_rate = 15
+
+tax_value = meal * tax_rate/100
+meal_with_tax = meal + tax_value
+tip_value = meal * tip_rate/100
+total_cost = meal_with_tax + tip_value
+
+puts "The pre-tax cost of your meal is $%.2f." % meal
+puts "At a tax rate of %d%%, the total tax levied on this meal is $%.2f." % [tax_rate, tax_value]
+puts "Considering a tip rate of %d%%, we recommend you tip your waitress $%.2f." % [tip_rate, tip_value]
+puts "The total cost for this meal is $%.2f. Thanks." % [total_cost]
+
